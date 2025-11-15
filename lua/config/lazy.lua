c18dcdc6556d84d13fa67fdf7679088c18613777
saharshbhansali <saharsh.bhansali15@gmail.com>
2025-11-15 15:39:46 +0530

    fix: remove mini-animate plugin
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -67,7 +67,7 @@ require('lazy').setup {
         { import = 'lazyvim.plugins.extras.lsp.none-ls' },
         { import = 'lazyvim.plugins.extras.ui.dashboard-nvim' },
         { import = 'lazyvim.plugins.extras.ui.indent-blankline' },
-        { import = 'lazyvim.plugins.extras.ui.mini-animate' },
+        -- { import = 'lazyvim.plugins.extras.ui.mini-animate' },
         { import = 'lazyvim.plugins.extras.ui.mini-indentscope' },
         { import = 'lazyvim.plugins.extras.ui.treesitter-context' },
         { import = 'lazyvim.plugins.extras.util.dot' },
