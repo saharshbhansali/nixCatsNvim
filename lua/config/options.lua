29059f36ca4908a8915eb0afa21a89da3f4abc71
saharshbhansali <saharsh.bhansali15@gmail.com>
2025-11-15 15:40:06 +0530

    feat (wip): use snacks as the default picker
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.g.lazyvim_picker = "snacks"
+
 local opt = vim.opt
 
 opt.tabstop = 2
@@ -13,3 +16,4 @@ opt.clipboard = "unnamedplus"
 
 opt.pumblend = 0
 opt.winblend = 0
+
